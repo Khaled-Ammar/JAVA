1c4789fbefc4727b78bb5f93a6fad4fb5c922874
khaled ammar <khal00o00@outlook.sa>
2022-10-30 15:26:39 +0300

    zoo1

---
+++ Cafe64/Cafe.java
@@ -0,0 +1,21 @@
+class Cafe {
+    ArrayList <items> 
+    public Cafe(){
+        this.
+    }
+    public void newOrder() {
+        
+        System.out.println("Please enter customer name for new order:");
+        String name = System.console().readLine();
+        
+        System.out.println("Please enter a menu item index or q to quit:");
+        String itemNumber = System.console().readLine();
+        
+        
+        while(!itemNumber.equals("q")) {
+    
+        }
+        
+    }
+    
+}
---
+++ zoo1/src/Zoo.java
@@ -0,0 +1,13 @@
+
+public class Zoo {
+
+	public static void main(String[] args) {
+	Gorilla gor = new Gorilla();
+	gor.eatBananas();
+	gor.eatBananas();
+	gor.throwSomething();
+	gor.throwSomething();
+	gor.throwSomething();
+	gor.climb();
+	}
+	}
