ecdbe25405fba6b650c7ffe1087ac4c81f38fd0a
khaled-ammar <khal00o00@outlook.sa>
2022-11-02 17:44:43 +0200

    update

---
+++ sts/FruityLoops/src/main/java/com/khaled/FruityLoops/FruityLoopsApplication.java
@@ -0,0 +1,15 @@
+package com.khaled.FruityLoops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+
+@SpringBootApplication
+@Controller
+public class FruityLoopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FruityLoopsApplication.class, args);
+	}
+
+}
