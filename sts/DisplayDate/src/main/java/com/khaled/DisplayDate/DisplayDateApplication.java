7ed3890ace31990b6394dca3c15a69773c0d22a5
khaled-ammar <khal00o00@outlook.sa>
2022-11-02 14:40:16 +0200

    lastass

---
+++ sts/DisplayDate/src/main/java/com/khaled/DisplayDate/DisplayDateApplication.java
@@ -0,0 +1,16 @@
+package com.khaled.DisplayDate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+
+@SpringBootApplication
+@Controller
+public class DisplayDateApplication {
+	
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisplayDateApplication.class, args);
+	}
+
+}
